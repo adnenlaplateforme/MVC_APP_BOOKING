23089763bc2c22eb7b54b00f92c521d6f94071b0
adnenlaplateforme <adnen.said@laplateforme.io>
2025-10-17 14:26:58 +0200

    booking add function ok

--- controllers/booking_controller.php
+++ controllers/booking_controller.php
@@ -5,7 +5,8 @@ function booking_index()
     $data = [
         'title' => 'Liste de réservations',
         'message' => '',
-        'list_booking' => getAllBooking()
+        'days' => getCurrentWeek(),
+        'allbooking' => formatBookings(getAllBooking())
     ];
     load_view_with_layout('booking/index', $data);
 }
@@ -16,6 +17,45 @@ function booking_add()
         'title' => "Ajouter une réservation"
     ];
     if (is_post()) {
+        $title = clean_input(post('title'));
+        $description = clean_input(post('description'));
+        $time_start = clean_input(post('time_start'));
+        $time_end = clean_input(post('time_end'));
+        $date = clean_input(post('date'));
+        $csrf_token = post('csrf_token');
+
+        if (!isset($csrf_token) || $csrf_token !== $_SESSION['csrf_token']) {
+            set_flash('error', 'Formulaire invalide ou expiré.');
+            redirect('auth/register');
+            exit;
+        }
+        // Validation
+        if (empty($title) || empty($description) || empty($time_start) || empty($time_end) || empty($date)) {
+            set_flash('error', 'Tous les champs sont obligatoires.');
+        } else {
+            // AJouter une réservation
+            $date_start =  format_datetime($date, $time_start);
+            $date_end = format_datetime($date, $time_end);
+            $id_user = $_SESSION['user_id'];
+            $booking_id = insertBooking($title, $description, $date_start, $date_end, $id_user);
+            if ($booking_id) {
+                $msg = 'Réservation réussie !';
+                set_flash('success', $msg);
+                error_logging(ErrorType::Info, $msg);
+            } else {
+                $msg = "Erreur lors de l'ajout de réservation !";
+                set_flash("error", $msg);
+                error_logging(ErrorType::Error, $msg);
+            }
+        }
     }
     load_view_with_layout('booking/add', $data);
+}
+
+function booking_details()
+{
+    $data = [
+        'title' => 'Détails de la réservation'
+    ];
+    load_view_with_layout('booking/details', $data);
 }
