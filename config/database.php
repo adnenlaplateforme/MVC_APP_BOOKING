eb0629f80ab0f55d3b96601d19487d00ce9b9bab
adnenlaplateforme <adnen.said@laplateforme.io>
2025-10-17 14:29:28 +0200

    constante CRENEAUX ok

--- config/database.php
+++ config/database.php
@@ -21,3 +21,17 @@ define('INCLUDE_PATH', ROOT_PATH . '/includes');
 define('CORE_PATH', ROOT_PATH . '/core');
 define('PUBLIC_PATH', ROOT_PATH . '/public');
 define("LOG_PATH", ROOT_PATH . '/logs');
+
+define('CRENEAUX', array(
+    (object) ['name' => '8-9h', 'debut' => '08:00:00', 'fin' => '09:00:00'],
+    (object) ['name' => '9-10h', 'debut' => '09:00:00', 'fin' => '10:00:00'],
+    (object) ['name' => '10-11h', 'debut' => '10:00:00', 'fin' => '11:00:00'],
+    (object) ['name' => '11-12h', 'debut' => '11:00:00', 'fin' => '12:00:00'],
+    (object) ['name' => '12-13h', 'debut' => '12:00:00', 'fin' => '13:00:00'],
+    (object) ['name' => '13-14h', 'debut' => '13:00:00', 'fin' => '14:00:00'],
+    (object) ['name' => '14-15h', 'debut' => '14:00:00', 'fin' => '15:00:00'],
+    (object) ['name' => '15-16h', 'debut' => '15:00:00', 'fin' => '16:00:00'],
+    (object) ['name' => '16-17h', 'debut' => '16:00:00', 'fin' => '17:00:00'],
+    (object) ['name' => '17-18h', 'debut' => '17:00:00', 'fin' => '18:00:00'],
+    (object) ['name' => '18-19h', 'debut' => '18:00:00', 'fin' => '19:00:00'],
+));
